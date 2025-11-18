1548ad95243ea5b3ba5cdf312c82792c665a1d69
riguang <zscboy@gmail.com>
2025-11-18 11:12:11 +0800

    close udp proxy

--- src/tunnel/udp_proxy.rs
+++ src/tunnel/udp_proxy.rs
@@ -4,6 +4,7 @@ use tokio::net::UdpSocket;
 use tokio::sync::Mutex;
 use anyhow::Result;
 use log::{debug, error};
+use tokio::sync::{Notify};
 
 use crate::tunnel::tunnel::Tunnel;
 
@@ -15,6 +16,7 @@ pub struct UdpProxy {
 
     /// The timestamp of the most recent read or write activity
     pub last_active: Arc<Mutex<Instant>>,
+    pub closed_notify: Arc<Notify>,
 }
 
 impl UdpProxy {
@@ -24,12 +26,13 @@ impl UdpProxy {
             socket,
             timeout_secs,
             last_active: Arc::new(Mutex::new(Instant::now())),
+            closed_notify: Arc::new(Notify::new()),
         }
     }
 
     pub async fn destroy(&self) {
-        let socket_to_close = self.socket.clone();
-        drop(socket_to_close);
+        debug!("udp proxy {} destroy called", self.id);
+        self.closed_notify.notify_waiters();
     }
 
     /// Write data and update last_active
@@ -53,29 +56,34 @@ impl UdpProxy {
             );
 
             // Close socket
-            let socket_to_close = self.socket.clone();
-            drop(socket_to_close);
+            self.closed_notify.notify_waiters();
         }
     }
 
     pub async fn serve(self: Arc<Self>, tunnel: Arc<Tunnel>) -> Result<()> {
         let mut buf = vec![0u8; 4096];
-        loop {
-            match self.socket.recv_from(&mut buf).await {
-                Ok((n, _from)) => {
-                    {
-                        let mut t = self.last_active.lock().await;
-                        *t = Instant::now();
-                    }
+         loop {
+            tokio::select! {
+                recv_res = self.socket.recv_from(&mut buf) => {
+                    match recv_res {
+                        Ok((n, _from)) => {
+                            // update last_active
+                            let mut t = self.last_active.lock().await;
+                            *t = Instant::now();
 
-                    if let Err(e) =
-                        tunnel.on_proxy_udp_data_from_proxy(&self.id, &buf[..n]).await
-                    {
-                        error!("on_proxy_udp_data_from_proxy error: {}", e);
+                            if let Err(e) = tunnel.on_proxy_udp_data_from_proxy(&self.id, &buf[..n]).await {
+                                error!("on_proxy_udp_data_from_proxy error: {}", e);
+                            }
+                        }
+                        Err(e) => {
+                            debug!("udp proxy {} recv_from error: {:?}", self.id, e);
+                            break;
+                        }
                     }
                 }
-                Err(e) => {
-                    debug!("UdpProxy.serve recv_from error: {:?}", e);
+
+                _ = self.closed_notify.notified() => {
+                    debug!("udp proxy {} closing via manual destroy", self.id);
                     break;
                 }
             }
@@ -85,7 +93,7 @@ impl UdpProxy {
             error!("on_proxy_udp_close error: {}", e);
         }
 
-         debug!("udp proxy {} close", self.id);
+        debug!("udp proxy {} close", self.id);
 
         Ok(())
     }
